ede772f8510b8ca936d2afdbf4f26286d4781aba
MatiasAntilef <matias.romero.dv@gmail.com>
2026-04-15 10:41:05 -0400

    use router Link

--- src/app/shared/layouts/public-layout/public-layout.component.ts
+++ src/app/shared/layouts/public-layout/public-layout.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, RouterLinkWithHref } from '@angular/router';
 
 @Component({
   selector: 'app-public-layout',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterLinkWithHref],
   templateUrl: './public-layout.component.html',
   styleUrl: './public-layout.component.css',
 })
